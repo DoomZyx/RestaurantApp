563b60b4cde4c4d36d0e18a8a48e4a8fbf654f8e
DoomZyx <axelcella.ac@gmail.com>
2025-11-20 16:39:31 +0000

    fix(connection.js: -Ajout de l'asynchrnocité dans la fonction handleWebSocketConnection

--- Websocket/connection.js
+++ Websocket/connection.js
@@ -58,7 +58,7 @@ export async function handleWebSocketConnection(connection, request) {
     // ==========================================
     // HANDLER MESSAGES TWILIO
     // ==========================================
-    connection.on("message", (message) => {
+    connection.on("message", async (message) => {
       try {
         const data = JSON.parse(message.toString());
 
